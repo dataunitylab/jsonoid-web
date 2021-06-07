86b80efb53dd9c0381b48b3ae407e70f76597b96
Michael Mior <mmior@cs.rit.edu>
2021-06-07 11:26:58 -0400

    Allow use of preloaded examples

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 [![codecov](https://codecov.io/gh/michaelmior/jsonoid-web/branch/main/graph/badge.svg?token=uHGhS4yEOr)](https://codecov.io/gh/michaelmior/jsonoid-web)
 
 Web interface for JSONoid.
+
+## Datasets
+
+* [GDP](http://api.worldbank.org/countries/USA/indicators/NY.GDP.MKTP.CD?per_page=5000&format=json)
+* [Nobel Prize](http://api.nobelprize.org/v1/prize.json)
+* [USGS - Earthquakes](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson)
+* [Rick and Morty characters](https://rickandmortyapi.com/api/character/)
+* [TVmaze](https://api.tvmaze.com/singlesearch/shows?q=mr-robot&embed=episodes)
--- ui/src/SchemaInput.js
+++ ui/src/SchemaInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useMutation } from 'react-fetching-library';
+import { useMutation, useParameterizedQuery } from 'react-fetching-library';
 
 import { setSchema } from './features/schemaSlice';
 
@@ -13,16 +13,46 @@ const inferSchemaAction = (jsonl) => ({
   body: jsonl,
 });
 
+const fetchExampleAction = (example) => ({
+  headers: {'Accept-Encoding': 'text/plain'},
+  method: 'GET',
+  endpoint: `/jsonl/${example}`,
+});
+
+
 function SchemaInput() {
   const [json, setJson] = useState('');
-  const { loading, mutate } = useMutation(inferSchemaAction);
+  const [example, setExample] = useState('');
+  const { loading: inferLoading, mutate } = useMutation(inferSchemaAction);
+  const { loading: exampleLoading, query } = useParameterizedQuery(fetchExampleAction);
   const dispatch = useDispatch()
 
   return (
     <div className='SchemaInput'>
+      <div>Select an example schema:</div>
+      <select disabled={inferLoading || exampleLoading} value={example} onChange={async (e) => {
+        setExample(e.target.value);
+        const {error: queryError, payload: exampleJson} = await query(e.target.value);
+        if (queryError) {
+          alert('Failed to load example');
+          console.error(queryError);
+        } else {
+          setJson(exampleJson);
+        }
+      }}>
+        <option value="">(None)</option>
+        <option value="earthquakes.jsonl">USGS - Earthquakes</option>
+        <option value="nobel.jsonl">Nobel Prize</option>
+        <option value="rickandmorty.jsonl">Rick and Morty characters</option>
+        <option value="mr-robot.jsonl">Mr. Robot (TVmaze)</option>
+      </select>
+      <div style={{textAlign: 'center'}}>or</div>
       <div>Enter a collection of JSON documents (one per line):</div>
-      <textarea disabled={loading} value={json} onChange={e => setJson(e.target.value)} />
-        <button disabled={loading} onClick={async () => {
+        <textarea disabled={inferLoading} value={json} onChange={e => {
+          setExample('');
+          setJson(e.target.value);
+        }} />
+        <button disabled={inferLoading || exampleLoading} onClick={async () => {
           const {error: mutationError, payload: schema} = await mutate(json);
 
           if (mutationError) {
