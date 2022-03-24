619b9152d5ea38651c3710f206c9ff74ca8b772f
Michael Mior <mmior@cs.rit.edu>
2022-03-24 12:19:21 -0400

    Bump to jsonoid-discovery 0.9.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   // Runtime
-  lazy val jsonoid             = "edu.rit.cs"                 %% "jsonoid-discovery"      % "0.4.2"
+  lazy val jsonoid             = "edu.rit.cs"                 %% "jsonoid-discovery"      % "0.9.7"
 
   // Test
   lazy val scalaTest           = "org.scalatestplus.play"     %% "scalatestplus-play"     % "4.0.0"
